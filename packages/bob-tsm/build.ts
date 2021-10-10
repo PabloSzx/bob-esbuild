9e02b664d6802d245566f045ca1ed7ebbc2807cd
Pablo Sáez <pablosaez1995@gmail.com>
2021-10-09 22:58:58 -0300

    fix prepack script

--- packages/bob-tsm/build.ts
+++ packages/bob-tsm/build.ts
@@ -1,29 +1,36 @@
 import { buildCode } from '../bob-ts/src/build';
 import pkg from './package.json';
 
-await buildCode({
-  entryPoints: ['./src/require.ts', './src/bin.ts'],
-  clean: true,
-  format: 'cjs',
-  outDir: 'lib',
-  target: 'node13.2',
-  esbuild: {
-    define: {
-      VERSION: JSON.stringify(pkg.version),
+async function main() {
+  await buildCode({
+    entryPoints: ['./src/require.ts', './src/bin.ts'],
+    clean: true,
+    format: 'cjs',
+    outDir: 'lib',
+    target: 'node13.2',
+    esbuild: {
+      define: {
+        VERSION: JSON.stringify(pkg.version),
+      },
+      minify: false,
     },
-    minify: false,
-  },
-  sourcemap: false,
-});
+    sourcemap: false,
+  });
 
-await buildCode({
-  entryPoints: ['./src/loader.ts'],
-  clean: false,
-  format: 'esm',
-  outDir: 'lib',
-  target: 'node13.2',
-  esbuild: {
-    minify: false,
-  },
-  sourcemap: false,
+  await buildCode({
+    entryPoints: ['./src/loader.ts'],
+    clean: false,
+    format: 'esm',
+    outDir: 'lib',
+    target: 'node13.2',
+    esbuild: {
+      minify: false,
+    },
+    sourcemap: false,
+  });
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
 });
