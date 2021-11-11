078402e1e2751c57101d92ad405bb05b347525ac
PabloSzx <pablosaez1995@gmail.com>
2021-11-11 02:38:32 -0300

    Improve released package

---
+++ .changeset/flat-items-tease.md
@@ -0,0 +1,5 @@
+---
+'bob-esbuild-plugin': patch
+---
+
+Improve released package
--- packages/esbuild-plugin/self-build.ts
+++ packages/esbuild-plugin/self-build.ts
@@ -1,6 +1,16 @@
+import { promises } from 'fs';
+import { rewritePackageJson } from '../bob/src/config/packageJson';
 import { buildTsc } from '../bob/src/tsc/build';
+import pkg from './package.json';
 
-buildTsc().catch(err => {
+async function main() {
+  await Promise.all([
+    promises.writeFile('./lib/package.json', JSON.stringify(rewritePackageJson(pkg, 'lib', process.cwd()), null, 2), 'utf8'),
+    buildTsc(),
+  ]);
+}
+
+main().catch(err => {
   console.error(err);
   process.exit(1);
 });
