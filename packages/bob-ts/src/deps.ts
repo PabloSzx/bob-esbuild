0786f01099588869cbeeea876de69642c205e4bd
Pablo Sáez <pablosaez1995@gmail.com>
2021-12-24 00:09:18 -0300

    update

--- packages/bob-ts/src/deps.ts
+++ packages/bob-ts/src/deps.ts
@@ -1,6 +1,8 @@
-export { program, Option } from 'commander';
-export { globby } from 'globby';
-export { default as del } from 'rollup-plugin-delete';
-export { default as tsconfigPaths } from 'rollup-plugin-tsconfig-paths';
-export { default as rollupJson } from '@rollup/plugin-json';
-export { default as externals } from 'rollup-plugin-node-externals';
+import { default as rollupJson } from '@rollup/plugin-json';
+import { Option, program } from 'commander';
+import { globby } from 'globby';
+import { default as del } from 'rollup-plugin-delete';
+import { default as externals } from 'rollup-plugin-node-externals';
+import { default as tsconfigPaths } from 'rollup-plugin-tsconfig-paths';
+
+export { program, Option, globby, del, tsconfigPaths, rollupJson, externals };
--- packages/bob/src/deps.ts
+++ packages/bob/src/deps.ts
@@ -1,17 +1,19 @@
-export { default as makePublishManifest } from '@pnpm/exportable-manifest';
-export { default as colors } from 'chalk';
+import { default as makePublishManifest } from '@pnpm/exportable-manifest';
+import { default as rollupJson } from '@rollup/plugin-json';
+import { default as colors } from 'chalk';
+import { default as format } from 'date-fns/format/index.js';
+import fsExtra from 'fs-extra';
+import { default as get } from 'lodash.get';
+import { default as del } from 'rollup-plugin-delete';
+import { default as externals } from 'rollup-plugin-node-externals';
+import { default as tsconfigPaths } from 'rollup-plugin-tsconfig-paths';
+import { default as treeKill } from 'tree-kill';
+import { parse as parseTsconfig } from 'tsconfck';
+
 export { cosmiconfig } from 'cosmiconfig';
-export { default as format } from 'date-fns/format/index.js';
 export { execaCommand as command } from 'execa';
 export { hashElement } from 'folder-hash';
 export { globby } from 'globby';
-export { default as get } from 'lodash.get';
-export { default as del } from 'rollup-plugin-delete';
-export { default as externals } from 'rollup-plugin-node-externals';
-export { default as tsconfigPaths } from 'rollup-plugin-tsconfig-paths';
-export { default as treeKill } from 'tree-kill';
-export { parse as parseTsconfig } from 'tsconfck';
-export { default as rollupJson } from '@rollup/plugin-json';
-import fsExtra from 'fs-extra';
+export { format, makePublishManifest, colors, get, del, externals, tsconfigPaths, treeKill, parseTsconfig, rollupJson };
 
 export const { copyFile, mkdirp, pathExists, readJSON, writeJSON, copy, ensureDir } = fsExtra;
